4e94f24cd5b73d07291e8b8b86f0ded9c802102f
cmkshaw <caroline.shaw@gmail.com>
2014-12-15 23:07:02 -0500

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,27 @@
+(function() {
+  "use strict";
+
+  console.log('Whatcha doin?');
+
+  var allQuotes = [
+    ["Here's my theory about meetings and life: the three things you can't fake are erections, competence and creativity – D. Coupland"],
+    ["Remember how, back in 1990, if you used a cellphone in public you looked like a total asshole? We're all assholes now. – D. Coupland"],
+    ["I want to stand as close to the edge as I can without going over. Out on the edge you see all kinds of things you can't see from the center – K. Vonnegut"],
+    ["Contemporary art hates you. – J. Waters"]
+  ];
+
+var randomNum = Math.floor(Math.random() * allQuotes.length)
+
+$('.quotation').text(allQuotes[randomNum]);
+
+var $container = $('#container');
+// initialize
+
+  $container.masonry({
+  	  columnWidth: 320,
+  	  gutterWidth:10,
+  	  itemSelector: '.item'
+  });
+
+
+}());
